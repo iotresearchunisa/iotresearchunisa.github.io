98502e155524afd9fc287124b74adda70e2a79f7
Gennaro Pio Rimoli <gennaropiorimoli@gmail.com>
2024-07-18 12:19:24 +0200

    refactor
    refactor

--- publications/main.py
+++ publications/main.py
@@ -4,13 +4,12 @@ import requests
 import bibtexparser
 from bs4 import BeautifulSoup
 
-def scarica_pagina(url):
-    response = requests.get(url)
-    return response.text
-def estrai_item_id(html_content):
+
+def extract_paper_ids(url):
+    html_content = requests.get(url).content.decode('utf-8')
     soup = BeautifulSoup(html_content, 'html.parser')
     input_elements = soup.find_all('input', attrs={'name': 'item_id'})
-    item_ids = set()  # Utilizziamo un insieme per evitare duplicati
+    item_ids = set()
 
     for element in input_elements:
         item_id = element.get('value')
@@ -20,11 +19,9 @@ def estrai_item_id(html_content):
     return item_ids
 
 
-def sort_by_year(entries):
-    return sorted(entries, key=lambda x: int(x['year']), reverse=True)
-
-
 def create_json(entries):
+    current_year = datetime.date.today().year
+
     data = {
         "conference": [],
         "book": [],
@@ -35,7 +32,7 @@ def create_json(entries):
     for entry in entries:
         if int(entry['year']) > current_year:
             entry['year'] = '0'
-		
+
         entry_type = entry.get('ENTRYTYPE', 'other').lower()
         if entry_type == 'inproceedings' or entry_type == 'conference':
             data['conference'].append(entry)
@@ -48,59 +45,43 @@ def create_json(entries):
 
     # Sorting each category in the data dictionary
     for category in data:
-        data[category] = sort_by_year(data[category])
+        data[category] = sorted(data[category], key=lambda x: int(x['year']), reverse=True)
 
     return data
 
 
-def save_json(data, file_path):
-    with open(file_path, 'w') as json_file:
-        json.dump(data, json_file, indent=4)
+def main():
+    profileIds = [
+        'rp107661', # Ficco
+        'rp11441',  # Palmieri
+        'rp17676',  # D'angelo
+        'rp17078',  # Esposito
+        'rp00030',  # Castiglione
+        'rp00885',  # Fiore
+        'rp135825', # Fusco
+        'rp79328',  # Eslam
+        'rp81635',  # Rimoli
+        'rp80959',  # Boi
+        # 'rp79354' # Leila
+    ]
 
-current_year = datetime.date.today().year
-profileIds = [
-    'rp107661', # Ficco
-    'rp11441',  # Palmieri
-    'rp17676',  # D'angelo
-    'rp17078',  # Esposito
-    'rp00030',  # Castiglione
-    'rp00885',  # Fiore
-    'rp135825', # Fusco
-    'rp79328',  # Eslam
-    'rp81635',  # Rimoli
-    'rp80959',  # Boi
-]
-# rp79354 Leila
+    paper_ids = set()
 
-risultati_senza_duplicati = set()
+    for id in profileIds:
+        paper_ids.update(extract_paper_ids('https://www.iris.unisa.it/cris/rp/{}?start=0&sortBy=-1&order=ASC&type=all&rpp=99999'.format(id)))
 
-for id in profileIds:
-    url = 'https://www.iris.unisa.it/cris/rp/{}?start=0&sortBy=-1&order=ASC&type=all&rpp=99999'.format(id)
-    html_content = scarica_pagina(url)
-    item_ids = estrai_item_id(html_content)
-    risultati_senza_duplicati.update(item_ids)
+    response = requests.post('https://www.iris.unisa.it/references.htm', data={
+        'format': 'bibtex',
+        'item_id': paper_ids
+    })
 
-#print("Item_id trovati")
-#set_str = '\n'.join(risultati_senza_duplicati)
-#file_path = 'items_ids.txt'
-#with open(file_path, 'w') as file:
-#    file.write(set_str)
+    if response.status_code == 200:
+        bib_database = bibtexparser.loads(response.content.decode('utf-8')).entries
+        with open('formatted_output.json', 'w') as json_file:
+            json.dump(create_json(bib_database), json_file, indent=4)
+    else:
+        print(f"Errore nella chiamata POST: {response.status_code} - {response.text}")
 
-post_url = 'https://www.iris.unisa.it/references.htm'
 
-data = {
-    'format': 'bibtex',
-    'item_id': risultati_senza_duplicati
-}
-
-response = requests.post(post_url, data=data)
-
-if response.status_code == 200:
-    bib_database = bibtexparser.loads(response.content.decode('utf-8')).entries
-    save_json(create_json(bib_database), 'formatted_output.json')
-
-#    with open('raw_output.bib', 'wb') as f:
-#        f.write(response.content)
-#    print("File CSV salvato correttamente.")
-else:
-    print(f"Errore nella chiamata POST: {response.status_code} - {response.text}")
+if __name__ == "__main__":
+    main()
